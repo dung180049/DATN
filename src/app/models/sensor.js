71b66c01734f90696d77a7497b4af26d47f7cd36
Viet Dung <vietdungmci@gmail.com>
2022-06-04 10:28:33 +0700

    change type of hour

--- src/app/models/sensor.js
+++ src/app/models/sensor.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const timeSave = new Date()
+var timeSave = new Date()
 
 const Sensor = new Schema({
     Month: { type: Number, require: true, default: timeSave.getMonth() + 1 },
     Date: { type: Number, require: true, default: timeSave.getDate() },
-    Hour: { type: Number, require: true, default: timeSave.getHours() },
+    Hour: { type: String, require: true, default: timeSave.toLocaleTimeString() },
     temperature: { type: Number, require: true },
     humidity: { type: Number, require: true },
 }, );
