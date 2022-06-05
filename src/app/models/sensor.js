66237f59f6dd1e4ae29dd14cb2f8ed7f35d3208b
Viet Dung <vietdungmci@gmail.com>
2022-06-05 08:23:03 +0700

    fix UTC GMT+7

--- src/app/models/sensor.js
+++ src/app/models/sensor.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var timeSave = new Date()
 
+var timeInMilliseconds = timeSave.getTime()
+var timeInUTC7 = timeInMilliseconds + (7 * 60 * 60 * 1000)
+var timeSaveInUTC7 = new Date(timeInUTC7)
+
 const Sensor = new Schema({
-    Month: { type: Number, require: true, default: timeSave.getMonth() + 1 },
-    Date: { type: Number, require: true, default: timeSave.getDate() },
-    Hour: { type: String, require: true, default: timeSave.toLocaleTimeString() },
+    Month: { type: Number, require: true, default: timeSaveInUTC7.getMonth() + 1 },
+    Date: { type: Number, require: true, default: timeSaveInUTC7.getDate() },
+    Hour: { type: Number, require: true, default: timeSaveInUTC7.getHours() },
     temperature: { type: Number, require: true },
     humidity: { type: Number, require: true },
 }, );
